d81703f1addd894c5ffb48a99834e06636d3987b
Nicolas <nicolas.lopez1919@gmail.com>
2025-06-29 16:08:26 -0300

    Initial commit from local testV1

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig}    */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mov|mp4|webm)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next",
+          name: "assets/videos/[name].[hash].[ext]",
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
